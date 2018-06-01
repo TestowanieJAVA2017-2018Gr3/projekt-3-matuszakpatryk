fbdeb3fc8ee2a58f3530639f7ebb16c41ace0605
Patryk Matuszak <matuszak.patrykk@gmail.com>
2018-06-01 21:39:09 +0200

    add phantomJS test

---
+++ src/test/java/PhantomJSFluentTests.java
@@ -0,0 +1,28 @@
+import io.github.bonigarcia.SeleniumExtension;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.openqa.selenium.phantomjs.PhantomJSDriver;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@ExtendWith(SeleniumExtension.class)
+public class PhantomJSFluentTests
+{
+
+    private PhantomJSDriver driver;
+
+    public PhantomJSFluentTests(PhantomJSDriver driver) {
+        this.driver = driver;
+    }
+
+    @BeforeEach
+    public void setDefaultPage() {
+        driver.get("https://duckduckgo.com/");
+    }
+
+    @Test
+    public void testTitlePage() {
+        assertEquals("DuckDuckGo — Privacy, simplified.", driver.getTitle());
+    }
+}
