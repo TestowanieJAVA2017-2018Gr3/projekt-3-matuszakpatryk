67c1edadd7a4a8e140e2c965fd9c36c536cff43b
Patryk Matuszak <matuszak.patrykk@gmail.com>
2018-06-03 21:24:40 +0200

    add opera tests

--- src/test/java/OperaTests.java
+++ src/test/java/OperaTests.java
@@ -5,7 +5,7 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.openqa.selenium.opera.OperaDriver;
 import org.openqa.selenium.support.PageFactory;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @ExtendWith(SeleniumExtension.class)
 public class OperaTests
@@ -20,6 +20,59 @@ public class OperaTests
     public void AddCustomerWithoutNameTest() throws Exception {
         AddCustomerPageObject page = PageFactory.initElements(driver, AddCustomerPageObject.class);
         page.SignIn("testtest@wp.pl", "Test123!");
-        assertTrue(page.SignInWithoutDataAssert());
+        page.GoToCustomerPageAndClickCreateButton();
+        page.InsertData("", "Dodany", "123-123-123", "dodajsie@prosze.com", "Moze", "Dodam");
+        assertEquals("Enter Name", page.CreateCustomerWithoutNameAssert());
+        driver.quit();
+    }
+
+    @Test
+    public void AddCustomerWithoutSurnameTest() throws Exception {
+        AddCustomerPageObject page = PageFactory.initElements(driver, AddCustomerPageObject.class);
+        page.SignIn("testtest@wp.pl", "Test123!");
+        page.GoToCustomerPageAndClickCreateButton();
+        page.InsertData("Dodany", "", "123-123-123", "dodajsie@prosze.com", "Moze", "Dodam");
+        assertEquals("Enter Surname", page.CreateCustomerWithoutSurnameAssert());
+        driver.quit();
+    }
+
+    @Test
+    public void AddCustomerWithoutPhoneTest() throws Exception {
+        AddCustomerPageObject page = PageFactory.initElements(driver, AddCustomerPageObject.class);
+        page.SignIn("testtest@wp.pl", "Test123!");
+        page.GoToCustomerPageAndClickCreateButton();
+        page.InsertData("Dodany", "Chyba", "", "dodajsie@prosze.com", "Moze", "Dodam");
+        assertEquals("Enter Phone", page.CreateCustomerWithoutPhoneAssert());
+        driver.quit();
+    }
+
+    @Test
+    public void AddCustomerWithoutEmailTest() throws Exception {
+        AddCustomerPageObject page = PageFactory.initElements(driver, AddCustomerPageObject.class);
+        page.SignIn("testtest@wp.pl", "Test123!");
+        page.GoToCustomerPageAndClickCreateButton();
+        page.InsertData("Dodany", "Chyba", "123-123-123", "", "Moze", "Dodam");
+        assertEquals("Enter Email", page.CreateCustomerWithoutEmailAssert());
+        driver.quit();
+    }
+
+    @Test
+    public void AddCustomerWithoutCityTest() throws Exception {
+        AddCustomerPageObject page = PageFactory.initElements(driver, AddCustomerPageObject.class);
+        page.SignIn("testtest@wp.pl", "Test123!");
+        page.GoToCustomerPageAndClickCreateButton();
+        page.InsertData("Dodany", "Chyba", "123-123-123", "dodajsie@prosze.com", "", "Dodam");
+        assertEquals("Enter City Name", page.CreateCustomerWithoutCityAssert());
+        driver.quit();
+    }
+
+    @Test
+    public void AddCustomerWithoutCountryTest() throws Exception {
+        AddCustomerPageObject page = PageFactory.initElements(driver, AddCustomerPageObject.class);
+        page.SignIn("testtest@wp.pl", "Test123!");
+        page.GoToCustomerPageAndClickCreateButton();
+        page.InsertData("Dodany", "Chyba", "123-123-123", "dodajsie@prosze.com", "Moze", "");
+        assertEquals("Enter Country Name", page.CreateCustomerWithoutCountryAssert());
+        driver.quit();
     }
 }
--- src/test/java/PageObjects/AddCustomerPageObject.java
+++ src/test/java/PageObjects/AddCustomerPageObject.java
@@ -1,5 +1,6 @@
 package PageObjects;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
@@ -16,6 +17,27 @@ public class AddCustomerPageObject
 
     @FindBy(name = "Password")
     private WebElement password;
+
+    @FindBy(id = "CreateCustomer")
+    private WebElement CreateCustomer;
+
+    @FindBy(id = "Name")
+    private WebElement Name;
+
+    @FindBy(id = "Surname")
+    private WebElement Surname;
+
+    @FindBy(id = "Phone")
+    private WebElement Phone;
+
+    @FindBy(id = "Email")
+    private WebElement Email;
+
+    @FindBy(id = "City")
+    private WebElement City;
+
+    @FindBy(id = "Country")
+    private WebElement Country;
 
     public AddCustomerPageObject(WebDriver driver)
     {
@@ -34,6 +56,47 @@ public class AddCustomerPageObject
     public void GoToCustomerPageAndClickCreateButton()
     {
         driver.get("http://flowershopapp.azurewebsites.net/customer");
+        CreateCustomer.click();
     }
 
+    public void InsertData(String name, String surname, String phone, String email, String city, String country)
+    {
+        Name.sendKeys(name);
+        Surname.sendKeys(surname);
+        Phone.sendKeys(phone);
+        Email.sendKeys(email);
+        City.sendKeys(city);
+        Country.sendKeys(country);
+        Country.submit();
+    }
+
+    public String CreateCustomerWithoutNameAssert()
+    {
+        return driver.findElement(By.xpath("//div[@class='form-group']/span")).getText();
+    }
+
+    public String CreateCustomerWithoutSurnameAssert()
+    {
+        return driver.findElement(By.xpath("(//div[@class='form-group'])[2]/span")).getText();
+    }
+
+    public String CreateCustomerWithoutPhoneAssert()
+    {
+        return driver.findElement(By.xpath("(//div[@class='form-group'])[3]/span")).getText();
+    }
+
+    public String CreateCustomerWithoutEmailAssert()
+    {
+        return driver.findElement(By.xpath("(//div[@class='form-group'])[4]/span")).getText();
+    }
+
+    public String CreateCustomerWithoutCountryAssert()
+    {
+        return driver.findElement(By.xpath("(//div[@class='form-group'])[5]/span")).getText();
+    }
+
+    public String CreateCustomerWithoutCityAssert()
+    {
+        return driver.findElement(By.xpath("(//div[@class='form-group'])[6]/span")).getText();
+    }
 }
